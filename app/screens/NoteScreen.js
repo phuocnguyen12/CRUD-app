9a1137c29682fcfecbbe4f3871492766d2411e90
Nguyễn Thiên Phước <83675378+phuocnguyen12@users.noreply.github.com>
2023-04-24 17:57:07 +0700

    Greetings change by time

--- app/screens/NoteScreen.js
+++ app/screens/NoteScreen.js
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text, StatusBar } from "react-native";
 import colors from "../misc/colors";
 
 const NoteScreen = ({ user }) => {
-  const [greet, setGreet] = useState('Morning');
+  const [greet, setGreet] = useState('');
+
+  const findGreet = () => {
+    const hrs = new Date().getHours();
+    if(hrs === 0 || hrs < 11) return setGreet('Morning');
+    if(hrs === 11 || hrs < 1) return setGreet('Noon');
+    if(hrs === 1 || hrs < 18) return setGreet('Afternoon');
+    if(hrs === 18 || hrs < 0) return setGreet('Evening');
+  };
+
+  useEffect(() => {
+    findGreet();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle='dark-content' backgroundColor={colors.PRIMARY} />
       <View style={styles.container}>
-        <Text>{`Good ${greet} ${user.name}`}</Text>
+        <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  header: {
+    frontSize: 25,
+    frontWeight: 'bold',
   },
 });
 
